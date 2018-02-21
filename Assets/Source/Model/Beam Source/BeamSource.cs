0921fff105add1016b08432136215cec303640f3
Sansari1 <sansari1@radford.edu>
2018-02-20 21:30:48 -0500

    Added basic raycast and line renderer
    Simply casts out and returns(?) what it hits. need to set up reflection, then diffraction, etc.

--- Assets/Source/Model/Beam Source/BeamSource.cs
+++ Assets/Source/Model/Beam Source/BeamSource.cs
@@ -2,6 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BeamSource : MonoBehaviour {
+public class BeamSource : MonoBehaviour
+{
+    private Ray beam;
+    private RaycastHit hit;
+    private LineRenderer visual;
+    void Start() {
+        visual = this.GetComponent<LineRenderer>();
+    }
+
+    void FixedUpdate() {
+        Vector3 endPos = new Vector3(0, 0, 0);
+        beam = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.green, 100);
+        if (Physics.Raycast(beam, out hit, 50)) {
+            endPos = hit.point;
+            visual.SetPosition(0, transform.position);
+            visual.SetPosition(1, endPos);
+        } else {
+            visual.SetPosition(0, transform.position);
+            visual.SetPosition(1, transform.forward * 20 + transform.position);
+        }
+        Debug.Log(endPos);
+
+    }
 
 }
