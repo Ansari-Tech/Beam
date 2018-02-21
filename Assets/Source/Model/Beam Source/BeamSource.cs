761c5a3d5606ff5ddf810616ab83875b9b7de7b8
Sansari1 <sansari1@radford.edu>
2018-02-20 21:55:17 -0500

    added bouncing
    ray will now bounce off "mirror" tagged objects. Currently, this recursive setup has the side effect of only drawing the most recent bounce, causing invisible lines.

--- Assets/Source/Model/Beam Source/BeamSource.cs
+++ Assets/Source/Model/Beam Source/BeamSource.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class BeamSource : MonoBehaviour
 {
-    private Ray beam;
-    private RaycastHit hit;
     private LineRenderer visual;
     void Start() {
         visual = this.GetComponent<LineRenderer>();
     }
 
     void FixedUpdate() {
+        sendRay(transform.position, transform.forward);
+    }
+
+    private void getRayHit(RaycastHit hit, Ray beam) {
+        switch (hit.collider.tag) {
+
+            case "mirror":
+                Debug.Log("hit a mirror");
+                sendRay(hit.transform.position, Vector3.Reflect(beam.direction, hit.normal));
+                break;
+
+        }
+    }
+
+    private void sendRay(Vector3 origin, Vector3 direction) {
+        Ray beam;
+        RaycastHit hit;
         Vector3 endPos = new Vector3(0, 0, 0);
-        beam = new Ray(transform.position, transform.forward);
-        Debug.DrawRay(transform.position, transform.forward * 100, Color.green, 100);
+        beam = new Ray(origin, direction);
+        // Debug.DrawRay(origin, direction * 100, Color.green, 100);
         if (Physics.Raycast(beam, out hit, 50)) {
             endPos = hit.point;
-            visual.SetPosition(0, transform.position);
+            visual.SetPosition(0, origin);
             visual.SetPosition(1, endPos);
         } else {
-            visual.SetPosition(0, transform.position);
-            visual.SetPosition(1, transform.forward * 20 + transform.position);
+            visual.SetPosition(0, origin);
+            visual.SetPosition(1, direction * 20 + origin);
         }
-        Debug.Log(endPos);
-
+        getRayHit(hit, beam);
     }
 
 }
