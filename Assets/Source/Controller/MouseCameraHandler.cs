bc156c62b8488b2f25acec3667abceb2bc23f5f7
Ansari <sajid@ansari.tech>
2018-09-19 09:30:37 -0400

    Moved Target from mobile to desktop
    +Added screen-edge movement
    -removed touch inputs from camera

--- Assets/Source/Controller/MouseCameraHandler.cs
+++ Assets/Source/Controller/MouseCameraHandler.cs
@@ -5,30 +5,57 @@ using UnityEngine;
 
 public class MouseCameraHandler : MonoBehaviour
 {
-
-    public float dragSpeed = 2;
-    private Vector3 dragOrigin;
-
-
+    [SerializeField]
+    private float screenEdgeOffset;
+    [SerializeField]
+    private float cameraSpeed;
+    [SerializeField]
+    private bool moving;
+    void Start()
+    {
+        screenEdgeOffset = .05f;
+        cameraSpeed = .2f;
+    }
     void Update()
     {
-        if (!GetComponent<MouseDrag>().isDragging())
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                dragOrigin = Input.mousePosition;
-                return;
-            }
-
-            if (!Input.GetMouseButton(0)) return;
-
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-            Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-
-            transform.Translate(move, Space.World);
-        }
-
+        moveCameraPosition(checkMousePosition(), moving);
     }
 
-
+    private void moveCameraPosition(Vector3 movePos, bool moving)
+    {
+        if (moving)
+        {
+            transform.Translate(checkMousePosition());
+        }
+    }
+    private Vector3 checkMousePosition()
+    {
+        Vector3 mouseDirection = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 result = new Vector3();
+        if (mouseDirection.x <= 0 + screenEdgeOffset)
+        {
+            moving = true;
+            result += new Vector3(-cameraSpeed, 0, 0);
+        }
+        else if (mouseDirection.x >= 1 - screenEdgeOffset)
+        {
+            moving = true;
+            result += new Vector3(cameraSpeed, 0, 0);
+        }
+        else if (mouseDirection.y <= 0 + screenEdgeOffset)
+        {
+            moving = true;
+            result += new Vector3(0, -cameraSpeed, 0);
+        }
+        else if (mouseDirection.y >= 1 - screenEdgeOffset)
+        {
+            moving = true;
+            result += new Vector3(0, cameraSpeed, 0);
+        }
+        else
+        {
+            moving = false;
+        }
+        return result;
+    }
 }
